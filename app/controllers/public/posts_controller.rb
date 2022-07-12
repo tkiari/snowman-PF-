1f3f5860cff692450aebce63d24ea5b0577307c1
tkiari <tanabe1261@gmail.com>
2022-07-12 05:18:19 +0000

    add post

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,51 @@
 class Public::PostsController < ApplicationController
   def index
+    @post = Post.new
+    @posts = Post.all
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to public_post_path(@post.id)
+    else
+    @posts = Post.all
+    @user = User.find(current_user.id)
+    render :index
+    end
   end
 
   def show
+    @post1 = Post.new
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to 
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    redirect_to public_post_path(@post.id)
+    else
+    render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:opinion,:image)
+  end
+
 end

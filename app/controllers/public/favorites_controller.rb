7eaed2895d3d119e8fe4e2e3a4e83133faffc645
tkiari <tanabe1261@gmail.com>
2022-07-27 12:58:37 +0000

    fix Ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,6 +5,11 @@ class Public::FavoritesController < ApplicationController
     @favorite = current_user.favorites.new(post_id: post.id)
     @favorite.save
     #redirect_to request.referer
+    
+    #タブ間での非同期通信（タブ切り替えの際に更新されるようにするため
+    @my_user = User.find(current_user.id)
+    #フォロー一覧
+    @users1 = @my_user.followings
   end
 
   def destroy
@@ -12,6 +17,11 @@ class Public::FavoritesController < ApplicationController
     @favorite = current_user.favorites.find_by(post_id: post.id)
     @favorite.destroy
     #redirect_to request.referer
+    
+    #タブ間での非同期通信（タブ切り替えの際に更新されるようにするため
+    @my_user = User.find(current_user.id)
+    #フォロー一覧
+    @users1 = @my_user.followings
   end
 
 end
--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -5,13 +5,23 @@ class Public::RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     @user = User.find(params[:user_id])
     #redirect_to request.referer
+
+#タブ間での非同期通信（タブ切り替えの際に更新されるようにするため
+    @my_user = User.find(current_user.id)
+    #フォロー一覧
+    @users1 = @my_user.followings
   end
-  
+
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
     @user = User.find(params[:user_id])
     #redirect_to request.referer
+
+#タブ間での非同期通信（タブ切り替えの際に更新されるようにするため
+    @my_user = User.find(current_user.id)
+    #フォロー一覧
+    @users1 = @my_user.followings
   end
 
 end
